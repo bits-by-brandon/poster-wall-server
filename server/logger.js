850b26e9bc62bb3297bfe1837e3d942d148dd4d5
chang <chang@purplerockscissors.com>
2018-09-09 12:13:54 -0400

    Changes logging to exclude file logging in development

--- server/logger.js
+++ server/logger.js
@@ -1,14 +1,19 @@
 const winston = require("winston");
-const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console(),
+
+let transports = [new winston.transports.Console()];
+
+// Adds file logging if in production
+if (process.env.NODE_ENV === "production") {
+  transports = transports.concat([
     new winston.transports.File({ filename: __dirname + "/logs/combined.log" }),
     new winston.transports.File({
       filename: __dirname + "/logs/errors.log",
       level: "error"
     })
-  ]
-});
+  ]);
+}
+
+const logger = winston.createLogger({ transports });
 
 /**
  * Generic function to node.js callback style errors
