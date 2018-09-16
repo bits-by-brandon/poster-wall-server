138305e810b4162f3197b9614860b3b7cee486ea
chang <chang@purplerockscissors.com>
2018-09-16 01:28:29 -0400

    Removes build as a post-deploy step

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:brandondc741/poster-wall-server.git',
       path : '/var/www/public/poster-wall.brandonchang.me',
-      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
